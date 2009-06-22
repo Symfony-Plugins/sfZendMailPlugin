3dd1b5bdcaaacb2622f3565227a67a3431df33fc
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-22 10:06:43 -0500

    Initial import
    git-svn-id: http://svn.symfony-project.com/plugins/sfZendMailPlugin/trunk@19462 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfZendMailPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfZendMailPlugin configuration.
+ * 
+ * @package     sfZendMailPlugin
+ * @subpackage  config
+ * @author      Benjamin Runnels <benjamin.r.runnels@citi.com>
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfZendMailPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
---
+++ test/unit/sfZendMailTest.php
@@ -0,0 +1,17 @@
+<?php
+include (dirname(__FILE__) . '/../bootstrap/unit.php');
+
+$t = new lime_test(2, new lime_output_color());
+
+$reflection_class = new ReflectionClass('Zend_Mail_Transport_Sendmail');
+$transport_class = $reflection_class->newInstanceArgs(array('-f thomas.rabaix@soleoweb.com'));
+
+$t->ok($transport_class instanceof Zend_Mail_Transport_Sendmail);
+
+$reflection_class = new ReflectionClass('Zend_Mail_Transport_Smtp');
+$transport_class = $reflection_class->newInstanceArgs(array(
+  '127.0.0.1', 
+  array('auth' => 'Plain')
+));
+
+$t->ok($transport_class instanceof Zend_Mail_Transport_Smtp);
