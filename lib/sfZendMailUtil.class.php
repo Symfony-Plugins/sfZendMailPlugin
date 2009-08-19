5c0c704cfdf0d2759016af6df853d813901f7bf3
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-19 13:51:27 -0500

    fixed a syntax bug in the sendMail method when executing the action
    git-svn-id: http://svn.symfony-project.com/plugins/sfZendMailPlugin/trunk@21254 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfZendMailUtil.class.php
+++ lib/sfZendMailUtil.class.php
@@ -5,7 +5,7 @@ class sfZendMailUtil
   private static $zendLoaded = false;
 
   /**
-   * Listen to the configuration.method_not_found to extends 
+   * Listen to the configuration.method_not_found to extends
    * the configuration object
    *
    * @param sfEvent $event
@@ -13,7 +13,7 @@ class sfZendMailUtil
   static function configurationMethodNotFound(sfEvent $event)
   {
     $params = $event->getParameters();
-    
+
     switch($params['method'])
     {
       case 'registerZend':
@@ -23,7 +23,7 @@ class sfZendMailUtil
   }
 
   /**
-   * Listen to the component.method_not_found to extends 
+   * Listen to the component.method_not_found to extends
    * the configuration object
    *
    * @param sfEvent $event
@@ -31,7 +31,7 @@ class sfZendMailUtil
   static function componentMethodNotFound(sfEvent $event)
   {
     $params = $event->getParameters();
-    
+
     switch($params['method'])
     {
       case 'sendEmail':
@@ -39,7 +39,7 @@ class sfZendMailUtil
         $event->setReturnValue(self::sendMailFromEvent($event));
         break;
     }
-  
+
   }
 
   /**
@@ -50,40 +50,40 @@ class sfZendMailUtil
   static function registerZend(sfEvent $event)
   {
     $event->setProcessed(true);
-    
+
     if(self::$zendLoaded)
     {
-      
+
       return;
     }
-    
+
     //If Zend isnt installed, lets make our own include path
     if (!class_exists('Zend_Loader')) {
         $zendPath = sfConfig::get('app_sf_zend_mail_framework_location');
         $chars = strlen($zendPath) - 1;
-        
+
         if ($zendPath[$chars] == '/') {
             $zendPath = substr($zendPath, 0, $chars);
         }
-        
+
         $zendLoaderPath = $zendPath . DIRECTORY_SEPARATOR . 'Zend' . DIRECTORY_SEPARATOR . 'Loader.php';
         if (!file_exists($zendLoaderPath)) {
             throw new LogicException('Cannot locate the Zend Framework installiation path');
         }
-        
+
         set_include_path($zendPath . PATH_SEPARATOR . get_include_path());
         require_once $zendLoaderPath;
     }
-    
+
     spl_autoload_register(array(
       'Zend_Loader', 'loadClass'
     ));
-    
+
     if(! sfAutoload::getInstance()->autoload('Zend_Loader'))
     {
       throw new LogicException('Zend Framework Library not found at : ' . sfConfig::get('app_sf_zend_mail_framework_location'));
     }
-    
+
     self::$zendLoaded = true;
   }
 
@@ -91,36 +91,36 @@ class sfZendMailUtil
   {
     $config = sfConfig::get('app_sf_zend_mail_config');
     $context = sfContext::getInstance();
-    
+
     // 2. REGISTER ZEND CLASS
     $context->getConfiguration()->registerZend();
-    
+
     // 3. CREATE THE ACTION
     $action = $context->getController()->getAction($moduleName, $actionName);
-    
+
     // check for a module config.php
     $moduleConfig = sfConfig::get('sf_app_module_dir') . '/' . $moduleName . '/config/config.php';
     if(is_readable($moduleConfig))
     {
       require_once ($moduleConfig);
     }
-    
+
     // 4. EXECUTE THE ACTION
     $action->getVarHolder()->add($vars);
-    if($action->execute($context->getRequest() == sfView::NONE))
+    if($action->execute($context->getRequest()) == sfView::NONE)
     {
       // someone has cancalled the email rendering
       return sfView::NONE;
     }
-    
+
     // 5. RENDER THE MAIL
     $view = new sfZendMailView($context, $moduleName, $actionName, 'sfZendMailView');
-    
+
     foreach($action->getVarHolder()->getAll() as $name => $value)
     {
       $view->setAttribute($name, $value);
     }
-    
+
     // define decorator
     if($config['decorator']['enabled'])
     {
@@ -131,51 +131,51 @@ class sfZendMailUtil
     {
       $view->setDecorator(false);
     }
-    
+
     // text version
     try
     {
       $template = $actionName . 'Success.text.php';
       $template_dir = $context->getConfiguration()->getTemplateDir($moduleName, $template);
-      
+
       $view->setDirectory($template_dir);
       $view->setTemplate($template);
       if($view->isDecorator())
       {
         $view->setDecoratorTemplate($config['decorator']['template'] . '.text.php');
       }
-      
+
       $text_version = $view->render($action->getVarHolder()->getAll());
-      
+
       $action->mail->setBodyText($text_version, $config['charset'], $config['encoding']);
     }
     catch(sfRenderException $e)
     {}
-    
+
     // html version
     try
     {
       $template = $actionName . 'Success.html.php';
       $template_dir = $context->getConfiguration()->getTemplateDir($moduleName, $template);
-      
+
       $view->setDirectory($template_dir);
       $view->setTemplate($template);
       if($view->isDecorator())
       {
         $view->setDecoratorTemplate($config['decorator']['template'] . '.html.php');
       }
-      
+
       $html_version = $view->render($action->getVarHolder()->getAll());
-      
+
       $action->mail->setBodyHtml($html_version, $config['charset'], $config['encoding']);
     }
     catch(sfRenderException $e)
     {}
-    
+
     // 6. SEND THE MAIL
     $transport_class = $config['transport']['class'];
     $transport_settings = $config['transport']['parameters'];
-    
+
     if(! is_array($transport_settings))
     {
       $transport_settings = array(
@@ -184,23 +184,23 @@ class sfZendMailUtil
     }
 
     $reflection_class = new ReflectionClass($transport_class);
-    
+
     if($reflection_class->getName() != $transport_class)
     {
       throw new LogicException('Please configure the app.yml for sfZendMailPlugin');
     }
-    
+
     $transport_class = $reflection_class->newInstanceArgs($transport_settings);
-    
+
     $action->mail->send($transport_class);
-    
+
     return $action->mail;
   }
 
   static public function sendMailFromEvent(sfEvent $event)
   {
     $event->setProcessed(true);
-    
+
     $params = $event->getParameters();
     if(count($params['arguments']) == 3)
     {
@@ -211,7 +211,7 @@ class sfZendMailUtil
       list($moduleName, $actionName) = $params['arguments'];
       $vars = array();
     }
-    
+
     return self::sendMail($moduleName, $actionName, $vars);
   }
 }
